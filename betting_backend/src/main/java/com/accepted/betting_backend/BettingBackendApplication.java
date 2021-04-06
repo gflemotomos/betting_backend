f29c867a4afbb383725d7731a17e1405c5c5777b
gflemotomos <iflemotomos@gmail.com>
2021-04-06 13:01:43 +0300

    start spring boot project

---
+++ betting_backend/src/main/java/com/accepted/betting_backend/BettingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accepted.betting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingBackendApplication.class, args);
+	}
+
+}
---
+++ betting_backend/src/test/java/com/accepted/betting_backend/BettingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accepted.betting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
