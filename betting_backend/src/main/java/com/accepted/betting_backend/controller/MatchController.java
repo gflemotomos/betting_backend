f556849d90dd8775858909957b9b40001b970d4f
gflemotomos <iflemotomos@gmail.com>
2021-04-08 20:36:41 +0300

    added swagger documentation

--- betting_backend/src/main/java/com/accepted/betting_backend/controller/MatchController.java
+++ betting_backend/src/main/java/com/accepted/betting_backend/controller/MatchController.java
@@ -5,6 +5,7 @@ import com.accepted.betting_backend.domain.MatchOdds;
 import com.accepted.betting_backend.exception.NotFoundException;
 import com.accepted.betting_backend.service.MatchOddsService;
 import com.accepted.betting_backend.service.MatchService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +25,7 @@ public class MatchController {
         this.matchOddsService = matchOddsService;
     }
 
+    @Operation(summary = "Get a match by its id")
     @GetMapping(value = "/{id}")
     public ResponseEntity<Match> searchById(@PathVariable("id") long id) {
 
@@ -33,12 +35,14 @@ public class MatchController {
         return new ResponseEntity<>(match, HttpStatus.OK);
     }
 
+    @Operation(summary = "Add new match to the schedule")
     @PostMapping
     public ResponseEntity<Match> addMatch(@RequestBody Match match) {
         matchService.addMatch(match);
         return ResponseEntity.ok().body(match);
     }
 
+    @Operation(summary = "Update match info data")
     @PutMapping
     public ResponseEntity<Match> updateMatch(@RequestBody Match match) {
         matchService.updateMatch(match);
@@ -46,12 +50,14 @@ public class MatchController {
     }
 
 
+    @Operation(summary = "Delete a match by its id")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Long> deleteMatch(@PathVariable("id") long id) {
         matchService.deleteMatchById(id);
         return ResponseEntity.ok().body(id);
     }
 
+    @Operation(summary = "search an odd by its id")
     @GetMapping(value = "/odds/{id}")
     public ResponseEntity<MatchOdds> searchOddsById(@PathVariable("id") long id) {
 
@@ -61,12 +67,14 @@ public class MatchController {
         return new ResponseEntity<>(matchOdds, HttpStatus.OK);
     }
 
+    @Operation(summary = "Add an odd to a match")
     @PostMapping(value = "/odds")
     public ResponseEntity<MatchOdds> addOdds(@RequestBody MatchOdds matchOdds) {
         matchOddsService.addMatchOdd(matchOdds);
         return ResponseEntity.ok().body(matchOdds);
     }
 
+    @Operation(summary = "Update odds info")
     @PutMapping(value = "odds")
     public ResponseEntity<MatchOdds> updateOdds(@RequestBody MatchOdds matchOdds) {
         matchOddsService.updateMatchOdd(matchOdds);
@@ -74,6 +82,7 @@ public class MatchController {
     }
 
 
+    @Operation(summary = "Delete an odd by its id")
     @DeleteMapping(value = "/odds/{id}")
     public ResponseEntity<Long> deleteOdds(@PathVariable("id") long id) {
         matchOddsService.deleteMatchOddById(id);
